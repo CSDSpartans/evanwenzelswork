2a1d49891f48615d23e90dd108ada24afec079d4
ewenzel123 <ewenzel@student.csdspartans.org>
2017-03-16 11:15:23 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -32,7 +32,7 @@ print ()
 print ()
 print ("The total number of stanzas in this poem are: ",  stanza_counter)
 print ("The total number of lines in this file are: ", total_lines_in_file)
-print ("Tuesday Afternoon first appeared in the alblum Days of Future Passed in 1967.")
+print ('"Tuesday Afternoon" first appeared in the album Days of Future Passed in 1967.')
 print ("The members of The Moody Blues are Justin Hayward, John Lodge, and Graeme Edge.")
 
 file_input.close()
